f744d8c705d8f02327c90f55cef31f54e3db156b
Oliver Lonsdale <lonsdale1086@outlook.com>
2022-05-22 00:55:03 +0100

    the rest of the initial commit

---
+++ scripts.js
@@ -0,0 +1,59 @@
+let pageLocked = false
+let sentence = []
+let sentenceText = []
+let lockSentence = false
+
+document.querySelectorAll(".item").forEach( button => {
+  button.addEventListener("click", ev => {
+    let name = ""
+    if(ev.target.nodeName === 'BUTTON'){
+      name = ev.target.id
+    }
+    else if (ev.target.nodeName === 'IMG'){
+      name = ev.path[1].id
+    }
+    document.getElementById(`${name}Clip`).play()
+    sentence.push(document.getElementById(`${name}Clip`))
+    sentenceText.push(name)
+    sentenceDisplay.value = sentenceText.join(" ")
+  })
+})
+
+lockButton.addEventListener("click",ev => {
+  let password 
+  if(pageLocked){
+    pageLocked = !pageLocked
+    lockButton.innerHTML = '<img class="lockIcon" src="./resouces/unlocked.png">'
+  } else {
+    pageLocked = !pageLocked
+    lockButton.innerHTML = '<img class="lockIcon" src="./resouces/locked.png">'
+  } 
+})
+
+document.getElementById("clearButton").addEventListener("click", ev=> {
+  if(lockSentence) return
+  sentence = []
+  sentenceText = []
+  sentenceDisplay.value = sentenceText.join(" ")
+})
+
+document.getElementById("playButton").addEventListener("click", ev=> {
+  playSentence()
+})
+
+async function playSentence(i = 0, recurse = false){
+  if(lockSentence && !recurse) return
+  lockSentence = true
+  if(i == sentence.length){
+    lockSentence = false
+    sentence = []
+    sentenceText = []
+    sentenceDisplay.value = sentenceText.join(" ")
+    return
+  }
+  sentence[i].play()
+  //seconds -> ms -> plus gap between words
+  let timeout = sentence[i].duration * 1000
+  i++
+  setTimeout(() => {playSentence(i, true)}, timeout)
+}
