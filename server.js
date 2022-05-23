0c82662827e3cb695f28df1c76ff6c72afbe4c01
Oliver Lonsdale <lonsdale1086@outlook.com>
2022-05-23 14:08:23 +0100

    imported logger

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const path = require("path")
+const logger = require("morgan")
 
 const app = express()
 
-app.use(morgan("short"))
+app.use(logger("short"))
 
 app.use(express.static(path.join(__dirname,"public")))
 
