0987ac49a86f401c1ce121cb41b22ca06a64a59c
lonsdale1086 <lonsdale1086@outlook.com>
2022-05-27 03:05:20 +0100

    Grids now generated from json to allow for pronounciation and icon names etc Added readme Added new icons

--- public/scripts.js
+++ public/scripts.js
@@ -1,654 +1,6 @@
 "use strict"
-const boards = [{ // LITTLE
-    name: "board0",
-    columns: 6,
-    tiles: [
-      ["want", "green"],
-      ["more", "blue"],
-      ["not", "grey"],
-      ["go", "green"],
-      ["help", "green"],
-      ["look", "green"],
-      ["play", "green"],
-      ["same", "blue"],
-      ["different", "blue"],
-      ["what", "pink"],
-      ["like", "green"],
-      ["stop", "green"],
-      ["drink", "green"],
-      ["eat", "green"],
-      ["turn", "grey"],
-      ["up", "red"],
-      ["down", "red"],
-      ["finish", "green"],
-    ]
-  },
-  { // MAIN
-    name: "board1",
-    columns: 9,
-    master: true,
-    links: [
-      ["Toys", 2],
-      ["Learn", 3],
-      ["Topic", 4],
-      ["Body", 5],
-      ["Home", 6],
-      ["Food", 7],
-      ["Drinks", 8],
-      ["People", 9],
-      ["Feelings", 10]
-    ],
-    tiles: [
-      ["I", "yellow"],
-      ["want", "green"],
-      ["more", "blue"],
-      ["not", "grey"],
-      ["go", "green"],
-      ["help", "green"],
-      ["look", "green"],
-      ["need", "green"],
-      ["have", "green"],
-      ["you", "yellow"],
-      ["play", "green"],
-      ["same", "blue"],
-      ["different", "blue"],
-      ["what", "pink"],
-      ["like", "green"],
-      ["stop", "green"],
-      ["where", "pink"],
-      ["in", "red"],
-      ["he", "yellow"],
-      ["eat", "green"],
-      ["drink", "green"],
-      ["turn", "green"],
-      ["up", "red"],
-      ["down", "red"],
-      ["finish", "green"],
-      ["who", "pink"],
-      ["on", "red"],
-      ["she", "yellow"],
-      ["do", "green"],
-      ["loud", "blue"],
-      ["quiet", "blue"],
-      ["fast", "blue"],
-      ["slow", "blue"],
-      ["funny", "blue"],
-      ["talk", "green"],
-      ["off", "red"],
-      ["it", "yellow"],
-      ["put", "green"],
-      ["make", "green"],
-      ["feel", "green"],
-      ["think", "green"],
-      ["is", "green"],
-      ["close", "green"],
-      ["open", "green"],
-      ["here", "grey"],
-    ]
-  },
-  { // TOYS
-    name: "board2",
-    columns: 9,
-    sub: true,
-    source: 1,
-    tiles: [
-      ["toy", "grey"],
-      ["game", "grey"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["computer", "grey"],
-      ["tablet", "grey"],
-      ["phone", "grey"], //one missing here for back button
-      ["ball", "grey"],
-      ["bubbles", "grey"],
-      ["balloon", "grey"],
-      ["blocks", "grey"],
-      ["foam", "grey"],
-      ["spinner", "grey"],
-      ["yo-yo", "grey"],
-      ["stretch bands", "grey"],
-      ["wind up", "grey"],
-      ["jigsaw", "grey"],
-      ["mr potato", "grey"],
-      ["dice", "grey"],
-      ["doll", "grey"],
-      ["teddy", "grey"],
-      ["board game", "grey"],
-      ["book", "grey"],
-      ["-", "-"],
-      ["-", "-"],
-      ["sing", "green"],
-      ["music", "grey"],
-      ["dance", "green"],
-      ["clap", "green"],
-      ["tickle", "green"],
-      ["push", "green"],
-      ["climb", "green"],
-      ["read", "green"],
-      ["-", "-"],
-      ["sand pit", "grey"],
-      ["water tray", "grey"],
-      ["swing", "grey"],
-      ["slide", "grey"],
-      ["roundabout", "grey"],
-      ["seesaw", "grey"],
-      ["ladder", "grey"],
-      ["-", "-"],
-      ["-", "-"],
-      ["bike", "grey"],
-      ["scooter", "grey"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-    ]
-  },
-  { // LEARN
-    name: "board3",
-    columns: 9,
-    sub: true,
-    source: 1,
-    tiles: [
-      /*[back]*/
-      ["⠀0⠀", "grey"],
-      ["add", "grey"],
-      ["minus", "grey"],
-      ["divide", "grey"],
-      ["times", "grey"],
-      ["equals", "grey"],
-      ["-", "-"],
-      ["count", "grey"],
-      ["⠀1⠀", "grey"],
-      ["⠀2⠀", "grey"],
-      ["⠀3⠀", "grey"],
-      ["⠀4⠀", "grey"],
-      ["⠀5⠀", "grey"],
-      ["⠀6⠀", "grey"],
-      ["⠀7⠀", "grey"],
-      ["⠀8⠀", "grey"],
-      ["⠀9⠀", "grey"],
-      ["learn", "green"],
-      ["cut", "green"],
-      ["colour", "green"],
-      ["paint", "green"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["red", "red"],
-      ["orange", "red"],
-      ["yellow", "red"],
-      ["green", "red"],
-      ["blue", "red"],
-      ["purple", "red"],
-      ["pink", "red"],
-      ["black", "red"],
-      ["brown", "red"],
-      ["stripes", "red"],
-      ["dots", "red"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["grey", "red"],
-      ["pen", "grey"],
-      ["pencil", "grey"],
-      ["scissors", "grey"],
-      ["sharpener", "grey"],
-      ["ruler", "grey"],
-      ["glitter", "grey"],
-      ["light", "grey"],
-      ["-", "-"],
-      ["dark", "grey"],
-    ]
-  },
-  { // TOPIC
-    name: "board4",
-    columns: 9,
-    sub: true,
-    source: 1,
-    tiles: [
-      /*[back]*/
-      ["animal", "grey"],
-      ["pet", "grey"],
-      ["zoo", "grey"],
-      ["farm", "grey"],
-      ["wild", "grey"],
-      ["fluffy", "pink"],
-      ["-", "-"],
-      ["big", "pink"],
-      ["cat", "grey"],
-      ["dog", "grey"],
-      ["rabbit", "grey"],
-      ["hamster", "grey"],
-      ["lizard", "grey"],
-      ["tortoise", "grey"],
-      ["snake", "grey"],
-      ["bird", "grey"],
-      ["-", "-"],
-      ["cow", "grey"],
-      ["sheep", "grey"],
-      ["chicken", "grey"],
-      ["horse", "grey"],
-      ["pig", "grey"],
-      ["elephant", "grey"],
-      ["spider", "grey"],
-      ["bug", "grey"],
-      ["small", "pink"],
-      ["car", "grey"],
-      ["bus", "grey"],
-      ["train", "grey"],
-      ["motorbike", "grey"],
-      ["plane", "grey"],
-      ["helicopter", "grey"],
-      ["boat", "grey"],
-      ["paw", "grey"],
-      ["tail", "grey"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["claw", "grey"],
-      ["-", "-"],
-      ["wheel", "grey"],
-      ["-", "-"],
-      ["left", "pink"],
-      ["-", "-"],
-      ["right", "pink"],
-      ["-", "-"],
-      ["drive", "green"],
-      ["fly", "green"],
-      ["ride", "green"],
-    ]
-  },
-  { // BODY
-    name: "board5",
-    columns: 9,
-    sub: true,
-    source: 1,
-    tiles: [
-      /*[back]*/
-      ["body", "grey"],
-      ["poo", "grey"],
-      ["wee", "grey"],
-      ["hurt", "grey"],
-      ["blood", "grey"],
-      ["itch", "grey"],
-      ["broken", "grey"],
-      ["bruise", "grey"],
-      ["-", "-"],
-      ["-", "-"],
-      ["nappy", "grey"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["finger", "grey"],
-      ["hand", "grey"],
-      ["arm", "grey"],
-      ["elbow", "grey"],
-      ["tummy", "grey"],
-      ["private parts", "grey"],
-      ["eye", "grey"],
-      ["nose", "grey"],
-      ["mouth", "grey"],
-      ["toe", "grey"],
-      ["foot", "grey"],
-      ["leg", "grey"],
-      ["knee", "grey"],
-      ["chest", "grey"],
-      ["head", "grey"],
-      ["ear", "grey"],
-      ["hair", "grey"],
-      ["tooth", "grey"],
-      ["pants", "grey"],
-      ["socks", "grey"],
-      ["shoe", "grey"],
-      ["trousers", "grey"],
-      ["shorts", "grey"],
-      ["shirt", "grey"],
-      ["skirt", "grey"],
-      ["tights", "grey"],
-      ["jumper", "grey"],
-      ["-", "-"],
-      ["zip", "grey"],
-      ["button", "grey"],
-      ["lace", "grey"],
-      ["velcro", "grey"],
-      ["-", "-"],
-      ["-", "-"],
-      ["coat", "grey"],
-      ["-", "-"],
-    ]
-  },
-  { // HOME
-    name: "board5",
-    columns: 9,
-    sub: true,
-    source: 1,
-    tiles: [
-      /*[back]*/
-      ["school", "blue"],
-      ["home", "blue"],
-      ["park", "blue"],
-      ["swimming pool", "blue"],
-      ["restaurant", "blue"],
-      ["shop", "blue"],
-      ["outside", "grey"],
-      ["weather", "pink"],
-      ["toilet", "blue"],
-      ["kitchen", "blue"],
-      ["living room", "blue"],
-      ["bedroom", "blue"],
-      ["bathroom", "blue"],
-      ["dining room", "blue"],
-      ["stairs", "blue"],
-      ["garden", "blue"],
-      ["rain", "grey"],
-      ["cooker", "grey"],
-      ["soap", "grey"],
-      ["chair", "grey"],
-      ["bed", "grey"],
-      ["swim", "green"],
-      ["table", "grey"],
-      ["buy", "green"],
-      ["window", "grey"],
-      ["sun", "grey"],
-      ["cook", "green"],
-      ["sponge", "grey"],
-      ["sofa", "grey"],
-      ["sleep", "green"],
-      ["bath", "grey"],
-      ["cutlery", "grey"],
-      ["money", "grey"],
-      ["door", "grey"],
-      ["cloud", "grey"],
-      ["-", "-"],
-      ["towel", "grey"],
-      ["TV", "grey"],
-      ["-", "-"],
-      ["sink", "grey"],
-      ["dish", "grey"],
-      ["-", "-"],
-      ["-", "-"],
-      ["mud", "grey"],
-      ["-", "-"],
-      ["wash", "green"],
-      ["sit", "green"],
-      ["-", "-"],
-      ["shower", "grey"],
-      ["-", "-"],
-      ["hot", "pink"],
-      ["cold", "pink"],
-      ["snow", "grey"],
-    ]
-  },
-  { // Food
-    name: "board6",
-    columns: 9,
-    sub: true,
-    source: 1,
-    tiles: [
-      /*[back]*/
-      ["food", "grey"],
-      ["-", "-"],
-      ["sauce", "grey"],
-      ["-", "-"],
-      ["breakfast", "grey"],
-      ["lunch", "grey"],
-      ["tea", "grey"],
-      ["snack", "grey"],
-      ["meal", "grey"],
-      ["mash", "grey"],
-      ["curry", "grey"],
-      ["rice", "grey"],
-      ["pasta", "grey"],
-      ["pizza", "grey"],
-      ["favourite", "pink"],
-      ["burned", "pink"],
-      ["healthy", "pink"],
-      ["veg", "grey"],
-      ["carrot", "grey"],
-      ["broccoli", "grey"],
-      ["potato", "grey"],
-      ["pea", "grey"],
-      ["bean", "grey"],
-      ["tomato", "grey"],
-      ["salad", "grey"],
-      ["bread", "grey"],
-      ["meat", "grey"],
-      ["fish", "grey"],
-      ["ham", "grey"],
-      ["chicken", "grey"],
-      ["tuna", "grey"],
-      ["nugget", "grey"],
-      ["burger", "grey"],
-      ["sausage", "grey"],
-      ["toast", "grey"],
-      ["cereal", "grey"],
-      ["egg", "grey"],
-      ["-", "-"],
-      ["cheese", "grey"],
-      ["chocolate", "grey"],
-      ["biscuit", "grey"],
-      ["crisp", "grey"],
-      ["chip", "grey"],
-      ["sandwich", "grey"],
-      ["soup", "grey"],
-      ["noodles", "grey"],
-      ["pancake", "grey"],
-      ["yogurt", "grey"],
-      ["butter", "grey"],
-      ["jam", "grey"],
-      ["sweet", "pink"],
-      ["spicy", "pink"],
-      ["wrap", "grey"],
-    ]
-  },
-  { // Drinks
-    name: "board7",
-    columns: 9,
-    sub: true,
-    source: 1,
-    tiles: [
-      /*[back]*/
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["hot", "blue"],
-      ["-", "-"],
-      ["cold", "blue"],
-      ["-", "-"],
-      ["-", "-"],
 
-      ["juice", "orange"],
-      ["water", "orange"],
-      ["milk", "orange"],
-      ["milkshake", "orange"],
-      ["fizzy", "orange"],
-      ["lemonade", "orange"],
-      ["cola", "orange"],
-      ["-", "-"],
-      ["-", "-"],
-
-      ["tea", "orange"],
-      ["coffee", "orange"],
-      ["hot chocolate", "orange"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-
-      ["fruit", "grey"],
-      ["apple", "grey"],
-      ["banana", "grey"],
-      ["orange", "grey"],
-      ["pear", "grey"],
-      ["lemon", "grey"],
-      ["grape", "grey"],
-      ["raisin", "grey"],
-      ["blackcurrant", "grey"],
-
-      ["strawberry", "grey"],
-      ["raspberry", "grey"],
-      ["blueberry", "grey"],
-      ["cherry", "grey"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-    ]
-  },
-  { // People
-    name: "board6",
-    columns: 9,
-    sub: true,
-    source: 1,
-    tiles: [
-      /*[back]*/
-      ["-", "-"],
-      ["-", "-"],
-      ["adult", "orange"],
-      ["child", "orange"],
-      ["-", "-"],
-      ["mum", "orange"],
-      ["dad", "orange"],
-      ["-", "-"],
-      ["man", "orange"],
-      ["boy", "orange"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["sister", "orange"],
-      ["brother", "orange"],
-      ["-", "-"],
-      ["woman", "orange"],
-      ["girl", "orange"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["grandma", "orange"],
-      ["grandad", "orange"],
-      ["-", "-"],
-      ["baby", "orange"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["nana", "orange"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["nice", "orange"],
-      ["-", "-"],
-      ["mean", "orange"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["his", "green"],
-      ["him", "green"],
-      ["her", "green"],
-      ["they", "green"],
-      ["them", "green"],
-      ["their", "green"],
-      ["old", "blue"],
-      ["-", "-"],
-      ["young", "blue"],
-    ]
-  },
-  { // Feelings
-    name: "board6",
-    columns: 9,
-    sub: true,
-    source: 1,
-    tiles: [
-      /*[back]*/
-      ["happy", "orange"],
-      ["sad", "orange"],
-      ["OK", "orange"],
-      ["smile", "green"],
-      ["sick", "orange"],
-      ["win", "green"],
-      ["easy", "green"],
-      ["angry", "orange"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["laugh", "green"],
-      ["-", "-"],
-      ["lose", "green"],
-      ["hard", "green"],
-      ["worried", "orange"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["frustrated", "orange"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["excited", "orange"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["embarrassed", "orange"],
-      ["birthday", "grey"],
-      ["christmas", "grey"],
-      ["easter", "grey"],
-      ["eid", "grey"],
-      ["ramadam", "grey"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-      ["-", "-"],
-    ]
-  },
-]
+import {boards} from "./boards.js"
 
 // Toys, Learn, Topic, Body, Home, Food, Drinks, People, Feelings
 
@@ -662,13 +14,13 @@ let selectedVoiceIndex
 
 //set 
 if (!localStorage.getItem("activeGrid")) {
-  localStorage.setItem("activeGrid", 0)
+  localStorage.setItem("activeGrid", "main")
 }
 
 
-function drawBoard(index) {
+function drawBoard(name) {
   const gridSection = document.getElementById("gridSection")
-  const board = boards[index]
+  const board = boards[name]
 
   //clear existing
   gridSection.replaceChildren();
@@ -682,70 +34,74 @@ function drawBoard(index) {
     gridSection.classList.remove("six-cols")
   }
 
-  //add row of links
-  if (board.master) {
-    board.links.forEach(link => {
-      const button = document.createElement("div")
-      button.innerHTML = link[0]
-      button.classList.add("item")
-      button.classList.add("linkItem")
-      button.innerHTML = link[0] + `<img class="icon" src="./resouces/icons/${link[0].toLowerCase()}.png"></img>`
-      button.addEventListener("click", _ev => {
-        drawBoard(link[1])
-      })
-      gridSection.append(button)
-    })
-  }
-
-  //add back button
-  if (board.sub) {
-    const button = document.createElement("div")
-    button.innerHTML = "Back" + `<img class="icon" src="./resouces/back.svg"></img>`
-    button.classList.add("item")
-    button.classList.add("linkItem")
-    button.addEventListener("click", _ev => drawBoard(board.source))
-    gridSection.append(button)
-  }
-
   board.tiles.forEach(tile => {
 
-    //add blank tiles
-    if (tile[0] === "-") {
-      let blank = document.createElement("div")
-      blank.classList.add("item")
-      gridSection.append(blank)
-      return //acts as continue
+    //create button
+    const tileElement = document.createElement("div")
+    tileElement.classList.add("item")
+
+    if( tile.type === "blank") {
+      
     }
 
-    //create button
-    const button = document.createElement("div")
-    button.classList.add("item")
-    button.classList.add(tile[1]) //sets colour
-    button.id = tile[0].replace(" ", "-")
-    button.innerHTML = tile[0] + `<img class="icon" src="./resouces/icons/${tile[0].replaceAll(" ","-")}.png"></img>`
+    else if ( tile.type === "link") {
+      tileElement.classList.add("linkItem")
+      tileElement.classList.add(tile.colour) //sets colour
+      tileElement.id = tile.displayName.replaceAll(" ","")
+      tileElement.innerHTML = tile.displayName + `<img class="icon" src="./resouces/icons/${tile.iconName}.png"></img>`
 
-    //play sound and add to sentence
-    button.addEventListener("click", ev => {
-      let name = tile[0]
+      tileElement.addEventListener("click", ev => {
+        drawBoard(tile.linkTo)
+      })
+
+    }
+
+    else if (tile.type === "textOnly"){
+      tileElement.classList.add("item")
+      tileElement.classList.add("largeText")
+      tileElement.classList.add(tile.colour) //sets colour
+      tileElement.id = tile.displayName.replaceAll(" ","")
+      tileElement.innerHTML = tile.displayName
+      //play sound and add to sentence
+      tileElement.addEventListener("click", ev => {
+      let name = tile.pronounciation ?? tile.displayName
       if (synth.speaking || synth.pending) {
         synth.cancel()
       }
       sentence.push(name)
-
+      
       updateSentence()
       let utterance = new SpeechSynthesisUtterance(name)
       if(voice) utterance.voice = voice
       synth.speak(utterance)
     })
+    
+  }
 
-    //navigate back to main page
-    if (board.sub === true) {
-      button.addEventListener("click", ev => {
-        drawBoard(board.source)
-      })
+  else {
+
+    tileElement.classList.add("item")
+    tileElement.classList.add(tile.colour) //sets colour
+    tileElement.id = tile.displayName.replaceAll(" ","")
+    tileElement.innerHTML = tile.displayName + `<img class="icon" src="./resouces/icons/${tile.iconName}.png"></img>`
+      
+    //play sound and add to sentence
+    tileElement.addEventListener("click", ev => {
+    let name = tile.pronounciation ?? tile.displayName
+    if (synth.speaking || synth.pending) {
+      synth.cancel()
     }
+    sentence.push(name)
+    
+    updateSentence()
+    let utterance = new SpeechSynthesisUtterance(name)
+    if(voice) utterance.voice = voice
+    synth.speak(utterance)
+  })
+  
+}
 
-    gridSection.append(button)
+    gridSection.append(tileElement)
   })
 }
 
@@ -799,10 +155,10 @@ function populateVoiceList() {
     });
   
 
-  var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+  let selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
   voiceSelect.innerHTML = '';
   for (let i = 0; i < voices.length; i++) {
-    var option = document.createElement('option');
+    let option = document.createElement('option');
     option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
 
     if (voices[i].default) {
---
+++ readme.md
@@ -0,0 +1,36 @@
+# An Open Source Augmentative and Alternative Communication Web App
+
+## How the project was described
+
+- A series of grids of tiles, containing a word and an icon.
+- You click an icon and the word is spoken.
+- Each tile has a background colour.
+
+## How the project turned out
+
+- When a word is clicked, it is added to a "sentence". The words in the sentence can be spoken together with the play button.
+- Not all words have icons.
+- The same word may be used in different contexts, and will require different icons. E.g orange the colour and orange the fruit.
+- Some icons are links to other grids, and those grids need to return you to the last grid upon clicking an item.
+- Some words need to be pronounced differently to how they are written.
+- Numbers need to be spoken not as individual words. adding 2 and 3 to the sentence should be spoken "twenty-three".
+- Not all grids are the same size.
+- The layout of the grids must be consistant accross devices.
+
+## Notes
+
+Icons from opensymbols.org
+
+Voices from the speech engine of each device
+
+Grids designed by Beth Lonsdale
+
+System developed in vanilla HTML, Javascript and CSS, using visual studio code.
+
+The server is an Express Server running on Heroku.
+
+Tiles grids are defined as JSON objects, and rendered on the client side via javascript.
+
+Icons are all PNG files stored on the server.
+
+[Live version](https://aacore.herokuapp.com/)
